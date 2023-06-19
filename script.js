7c2e42750fb3cb81dac708d04fc62895f3569cdb
Eli <69530035+elisvcodes@users.noreply.github.com>
2023-06-19 15:54:02 -0400

    added the ability to add todo's

--- script.js
+++ script.js
@@ -2,48 +2,81 @@ const timeOfDay = document.querySelector(".timeOfDay");
 const nonActiveTodoBox = document.querySelector(".not-active-box");
 const selectDueDate = document.querySelector(".todo-due-date-container");
 const todoForm = document.querySelector(".active-todo-box");
-const todos = [];
+const closeForm = document.querySelector(".close-btn");
+const activeTodos = [];
 const completedTodos = [];
 const date = new Date();
-let currentTime = date.getHours();
+const currentTime = date.getHours();
 
-/*
-    Check what time of the day
-        * if it is under 12PM, display good morning
-        * if it is between 12PM and 6PM, display good afternoon
-        * if it is after 6PM, display good night
- */
+// Display appropriate greeting based on the current time of the day
 
-if (currentTime < 12) {
-  timeOfDay.textContent = "morning";
-} else if (currentTime < 18) {
-  timeOfDay.textContent = "afternoon";
-} else {
-  timeOfDay.textContent = "night";
-}
+const setGreeting = () => {
+  if (currentTime < 12) {
+    timeOfDay.textContent = "morning";
+  } else if (currentTime < 18) {
+    timeOfDay.textContent = "afternoon";
+  } else {
+    timeOfDay.textContent = "night";
+  }
+};
 
 // Todo creation box section
 
-nonActiveTodoBox.addEventListener("click", (e) => {
-  const todoForm = document.querySelector(".active-todo-box");
+const handleNonActiveTodoBoxClick = (e) => {
   const todoName = document.querySelector(".todo-name");
 
   nonActiveTodoBox.classList.add("hidden");
   todoForm.classList.remove("hidden");
   todoName.focus();
-});
+};
 
-selectDueDate.addEventListener("click", () => {
+const handleDatePickerClick = () => {
   const dueDateField = document.querySelector(".due-date");
   dueDateField.classList.remove("hidden");
   selectDueDate.classList.add("hidden");
   dueDateField.focus();
-});
+};
 
-todoForm.addEventListener("submit", (e) => {
+const handleFormSubmission = (e) => {
   e.preventDefault();
-  const formData = new FormData(todoForm);
-  for (const [name, value] of formData.entries()) {
-    console.log(name, value);
+  const dueDateField = document.querySelector(".due-date");
+  const todoName = document.querySelector(".todo-name").value;
+  const todoDescription = document.querySelector(".todo-description").value;
+  const todoDueDate = document.querySelector(".due-date").value;
+
+  const todo = {
+    title: todoName,
+    description: todoDescription,
+    dueDate: todoDueDate,
+  };
+
+  // Do not trigger anything if fields are empty
+  if (!todoName || !todoDescription || !todoDueDate) {
+    return;
   }
-});
+
+  activeTodos.push(todo);
+
+  todoForm.reset();
+  dueDateField.classList.add("hidden");
+  selectDueDate.classList.remove("hidden");
+};
+
+// If a user closes the form, do some clean up
+const handleCloseForm = () => {
+  const dueDateField = document.querySelector(".due-date");
+  nonActiveTodoBox.classList.remove("hidden");
+  todoForm.classList.add("hidden");
+  dueDateField.classList.add("hidden");
+  selectDueDate.classList.remove("hidden");
+  todoForm.reset();
+};
+
+// Event handlers
+
+nonActiveTodoBox.addEventListener("click", handleNonActiveTodoBoxClick);
+selectDueDate.addEventListener("click", handleDatePickerClick);
+todoForm.addEventListener("submit", handleFormSubmission);
+closeForm.addEventListener("click", handleCloseForm);
+// On page init
+setGreeting();
